ac7f944a22844f0be5117a8c7342b22ffaa974bd
Shivank Kapur <115289871+ShivankK26@users.noreply.github.com>
2023-08-14 13:43:26 +0530

    Update chains.js

--- mwallet/src/chains.js
+++ mwallet/src/chains.js
@@ -1,14 +1,14 @@
 const Ethereum = {
     hex: '0x1',
     name: 'Ethereum',
-    rpcUrl: 'https://1rpc.io/eth',
+    rpcUrl: '',
     ticker: "ETH"
 };
 
 const MumbaiTestnet = {
     hex: '0x13881',
     name: 'Mumbai Testnet',
-    rpcUrl: 'https://polygon-mumbai.infura.io/v3/4458cf4d1689497b9a38b1d6bbf05e78',
+    rpcUrl: '',
     ticker: "MATIC"
 };
 
